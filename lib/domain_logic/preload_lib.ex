7fb30f31ccd7a7ab0848da3f73549168ed2c1531
Chris Douglas <dougo.chris@gmail.com>
2019-02-18 07:00:41 +1100

    preload list

--- lib/domain_logic/preload_lib.ex
+++ lib/domain_logic/preload_lib.ex
@@ -3,7 +3,9 @@ defmodule Linklab.DomainLogic.PreloadLib do
 
   import Ecto.Query
 
-  @callback preload_with(Ecto.Schema.t(), atom() | list(atom())) :: Ecto.Queryable.t()
+  @type preload :: atom() | list(atom()) | keyword()
+
+  @callback preload_with(Ecto.Schema.t(), preload()) :: Ecto.Queryable.t()
 
   defmacro __using__(_opts) do
     quote do
@@ -12,7 +14,7 @@ defmodule Linklab.DomainLogic.PreloadLib do
       @behaviour Linklab.DomainLogic.PreloadLib
 
       @impl true
-      @spec preload_with(Ecto.Queryable.t(), atom() | list(atom())) :: Ecto.Queryable.t()
+      @spec preload_with(Ecto.Queryable.t(), PreloadLib.preload) :: Ecto.Queryable.t()
       def preload_with(query, associations) do
         Linklab.DomainLogic.PreloadLib.__preload_builder__(query, associations)
       end
